ab06c562640e8275f82454d13895040f111ad68e
Sean-Williamsgit config --global user.email sean.r.williams85@gmail.comgit config --global user.name Sean-Williams <wilmslo@gmail.com>
2020-08-24 13:40:42 +0100

    feat: create request using URL session, create Product struct

---
+++ NateChallenge/Product.swift
@@ -0,0 +1,16 @@
+//
+//  Product.swift
+//  NateChallenge
+//
+//  Created by Sean Williams on 24/08/2020.
+//  Copyright © 2020 Sean Williams. All rights reserved.
+//
+
+import Foundation
+
+struct Product: Codable {
+    let title: String
+    let merchant: String
+    let url: String
+    let images: [String]
+}
--- NateChallenge/ViewController.swift
+++ NateChallenge/ViewController.swift
@@ -12,9 +12,34 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let baseURL = URL(string: "http://192.168.0.14:3000/products")
+        var request = URLRequest(url: baseURL!)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+
+        
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            guard error == nil else {
+                print(error?.localizedDescription as Any)
+                return
+            }
+            
+            let decoder = JSONDecoder()
+            
+            do {
+                let result = try decoder.decode([Product].self, from: data!)
+                print(result)
+            } catch {
+                print(error.localizedDescription as Any)
+            }
+            
+        }.resume()
+        
+        
+        
     }
 
-
+    
 }
 
