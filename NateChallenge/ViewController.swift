23b4b79f2f41a74189eb597744cf27f04a90465a
Sean-Williamsgit config --global user.email sean.r.williams85@gmail.comgit config --global user.name Sean-Williams <wilmslo@gmail.com>
2020-08-24 12:49:12 +0100

    Initial Commit

---
+++ NateChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NateChallenge
+//
+//  Created by Sean Williams on 24/08/2020.
+//  Copyright © 2020 Sean Williams. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
